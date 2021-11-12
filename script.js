dcb389c5166ea02ef586e4826047fdb39fd8a842
Lu <lucha.m94@gmail.com>
2021-11-12 18:27:53 -0300

    added UI

--- script.js
+++ script.js
@@ -1,100 +1,142 @@
-let cpuChoise = ["rock","paper","scissors"];
+const playerChoiceRock = document.getElementById('rock');
+const playerChoicePaper = document.getElementById('paper');
+const playerChoiceScissors = document.getElementById('scissors');
 
-function computerPlay(){
+const cpuChoiseRock = document.getElementById('crock');
+const cpuChoisePaper = document.getElementById('cpaper');
+const cpuChoiseScissors = document.getElementById('cscissors');
+
+let playerScore = document.querySelector('.p_score');
+let cpuScore = document.querySelector('.c_score');
+let status = document.querySelector('.status');
+
+
+let cpuChoise = ["rock","paper","scissors"];
+function computerSelection(){
     randomPlay = cpuChoise[Math.floor(Math.random()*3)];
     return(randomPlay);
 }
 
-function userPlay(){
-    userChoice = prompt ("choose! rock, paper, scissors!");
-    userChoice = userChoice.toLocaleLowerCase();
-    return (userChoice);
-}
-
-function playRound (playerSelection, computerSelection){
-    console.log("player chooses: " + playerSelection);
-    console.log("cpu chooses: " + computerSelection);
-
+function playRound(playerSelection, computerSelection){
     if (playerSelection == "rock"){
         if (computerSelection == "rock"){
-            alert ("It's a tie!");
-            win = "nobody";
-            return(win);            
-        } else if (computerSelection == "paper"){
-            alert ("You loose! paper beats rock");
-            win = "cpu";
-            return win;
-        } else if (computerSelection == "scissors"){
-            alert ("You win! rock beats scissors");
-            win = "player";
-            return win;            
+            cpuChoiseRock.style.backgroundColor = "#00E1FF";
+            status.textContent = 'its a tie';
+            status.style.color = "#ffffff";
         }
-    }
-
-    if (playerSelection == "paper"){
         if (computerSelection == "paper"){
-            alert ("It's a tie!");
-            win = "nobody";
-            return(win);
-        } else if (computerSelection == "scissors"){
-            alert ("You loose! scissors beats paper");
-            win = "cpu";
-            return win;
-        } else if (computerSelection == "rock"){
-            alert ("You win! paper beats rock");
-            win = "player";
-            return win;            
+            cpuChoisePaper.style.backgroundColor = "#00E1FF";
+            cpuPoints++;
+            cpuScore.textContent = '' + cpuPoints;
+            status.textContent = 'you loose :(';
+            status.style.color = "#CD0000";
         }
+        if (computerSelection == "scissors"){
+            cpuChoiseScissors.style.backgroundColor = "#00E1FF";
+            playerPoints++;
+            playerScore.textContent= '' + playerPoints;
+            status.textContent = 'you win! :)';
+            status.style.color = "#b8ff00";
+        }
+    }
+    if (playerSelection == "paper"){
+        if (computerSelection == "rock"){
+            cpuChoiseRock.style.backgroundColor = "#00E1FF";
+            playerPoints++;
+            playerScore.textContent= '' + playerPoints;
+            status.textContent = 'you win! :)';
+            status.style.color = "#b8ff00";  
+        }
+        if (computerSelection == "paper"){
+            cpuChoisePaper.style.backgroundColor = "#00E1FF";
+            status.textContent = 'its a tie';
+            status.style.color = "#ffffff";
+        }
+        if (computerSelection == "scissors"){
+            cpuChoiseScissors.style.backgroundColor = "#00E1FF";
+            cpuPoints++;
+            cpuScore.textContent = '' + cpuPoints;
+            status.textContent = 'you loose :(';
+            status.style.color = "#CD0000";
+        }
+    }
+    if (playerSelection == "scissors"){
+        if (computerSelection == "rock"){
+            cpuChoiseRock.style.backgroundColor = "#00E1FF";
+            cpuPoints++;
+            cpuScore.textContent = '' + cpuPoints;
+            status.textContent = 'you loose :(';
+            status.style.color = "#CD0000";
+        }
+        if (computerSelection == "paper"){
+            cpuChoisePaper.style.backgroundColor = "#00E1FF";
+            playerPoints++;
+            playerScore.textContent= '' + playerPoints;
+            status.textContent = 'you win! :)';
+            status.style.color = "#b8ff00";            
+        }
+        if (computerSelection == "scissors"){
+            cpuChoiseScissors.style.backgroundColor = "#00E1FF";
+            status.textContent = 'its a tie';
+            status.style.color = "#ffffff";
+        }
+    }
+}
+
+function playerClick(){
+    playerChoiceRock.addEventListener("click", () =>{
+        playerChoiceRock.style.backgroundColor = "#FF00A5";
+        playRound('rock', computerSelection());
+        remove();
+        check();
+    });
+    playerChoicePaper.addEventListener("click", () =>{
+        playerChoicePaper.style.backgroundColor = "#FF00A5";
+        playRound('paper', computerSelection());
+        remove();
+        check();
+    });
+    playerChoiceScissors.addEventListener("click", () =>{
+        playerChoiceScissors.style.backgroundColor = "#FF00A5";
+        playRound('scissors', computerSelection());
+        remove();
+        check();
+    });
+}
+
+function remove(){
+    setTimeout(function(){
+        playerChoicePaper.style.backgroundColor = '#2d2d2d';
+        playerChoiceRock.style.backgroundColor = '#2d2d2d';
+        playerChoiceScissors.style.backgroundColor = '#2d2d2d';
+        cpuChoiseRock.style.backgroundColor = '#2d2d2d';
+        cpuChoiseScissors.style.backgroundColor = '#2d2d2d';
+        cpuChoisePaper.style.backgroundColor = '#2d2d2d';
+    },800);
+}
+
+function check (){
+    if (cpuPoints == 5 || playerPoints == 5){
+        if (cpuPoints == 5){
+            status.textContent = 'you lost the game! lets play again!';
+        }
+        if (playerPoints == 5){
+            status.textContent = 'you won the game! lets play again!';
+        }
+        cpuScore.textContent = '0';
+        playerScore.textContent = '0';
+        cpuPoints=0;
+        playerPoints=0;
+
     }
 
-    if (playerSelection == "scissors"){
-        if (computerSelection == "scissors"){
-            alert ("It's a tie!");
-            win = "nobody";
-            return(win);
-        } else if (computerSelection == "rock"){
-            alert ("You loose! rock beats scissors");
-            win = "cpu";
-            return win;
-        } else if (computerSelection == "paper"){
-            alert ("You win! scissors beats paper");
-            win = "player";
-            return win;            
-        }
-    }
 }
 
 function game(){
-    let cpuPoints = 0;
-    let playerPoints = 0;
-    while (!over){
-        let playerSelection = userPlay();
-        let computerSelection = computerPlay();
-        let winner = playRound(playerSelection, computerSelection);
-        
-        if (winner == "player"){
-            playerPoints = playerPoints + 1;
-        } else if (winner == "cpu"){
-            cpuPoints = cpuPoints + 1;
-        }
-
-        console.log ("winner is: " + winner);
-        alert ("The score is you: " + playerPoints + " | the cpu: " + cpuPoints);
-        
-        if (playerPoints == 3 || cpuPoints ==3){
-            over = true;
-            if (playerPoints == 3){
-                alert ("You won this time! congratulations!  :)");
-            } else if (cpuPoints == 3){
-                alert ("You lost! Better luck next time! :(")
-            }
-        }
-    }    
+    playerClick();
 }
 
-let over = false;
-
-alert ("Let's play rock, papper, scissors!");
-alert ("To the best of five! Are you ready?");
+let cpuPoints = 0;
+let playerPoints = 0;
 
 game();
